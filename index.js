2aedb26658b70663b725f2e93bf98481b16fbc63
Ozgen Eren <ozgeneral@gmail.com>
2020-11-17 21:07:26 +0100

    fix: consider package.json in node_modules

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var finder = require('find-package-json');
-var f = finder(__dirname);
-global.base_dir = require('path').dirname(f.next().filename)
+const finder = require('find-package-json');
+const path = require('path');
+const f = finder(__dirname);
+const MAX_ITERATIONS = 10;
 
+done = false;
+cnt = 0;
+while(!done) {
+    next = f.next();
+    if (next.done) { break; }
+    base_dir = path.dirname(next.filename);
+
+    cnt++;
+    if (cnt > MAX_ITERATIONS) {
+        throw new Error(`Could not find the base_dir from ${__dirname} after ${MAX_ITERATIONS}`);
+    }
+}
 module.exports = function(path) {
     return base_dir + path;
 }
